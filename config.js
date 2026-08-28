4b594c48787aef03e70fa871d73a81da036d3485
RRRRRRRr44443 <131881166+RRRRRRRr44443@users.noreply.github.com>
2026-08-28 22:04:47 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+// Supabase Project URL and Publishable Key.
+// Не вставляй service_role key в браузер.
+window.SUPABASE_URL = "https://thpxqmdgafbcnsqanvhw.supabase.co";
+window.SUPABASE_PUBLISHABLE_KEY = "sb_publishable_9dLfTs0sJV-_JVBYNraE2g_V1J6VgJ4";
